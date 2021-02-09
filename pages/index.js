b68db05c6b38041e5336d1cd442f1740cd6ad0a0
Sterling Velazquez <SterlingVelazquez@users.noreply.github.com>
2021-02-09 13:33:44 -0500

    Fixed Button Issue

--- pages/index.js
+++ pages/index.js
@@ -91,170 +91,170 @@ class Home extends React.Component {
     var width = window.innerWidth;
     if (width > 2040) {
       if (div === "gallery") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:960,
           behavior:"smooth"
         })
       } else if (div === "about") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:3125,
           behavior:"smooth"
         })
       } else {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:6510,
           behavior:"smooth"
         })
       } 
     } else if (width > 1680) {
       if (div === "gallery") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:960,
           behavior:"smooth"
         })
       } else if (div === "about") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:2900,
           behavior:"smooth"
         })
       } else {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:6075,
           behavior:"smooth"
         })
       }
     } else if (width > 1390) {
       if (div === "gallery") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:960,
           behavior:"smooth"
         })
       } else if (div === "about") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:2900,
           behavior:"smooth"
         })
       } else {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:5975,
           behavior:"smooth"
         })
       } 
     } else if (width > 1112) {
       if (div === "gallery") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:960,
           behavior:"smooth"
         })
       } else if (div === "about") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:2760,
           behavior:"smooth"
         })
       } else {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:5600,
           behavior:"smooth"
         })
       } 
     } else if (width > 868) {
       if (div === "gallery") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:950,
           behavior:"smooth"
         })
       } else if (div === "about") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:2600,
           behavior:"smooth"
         })
       } else {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:5950,
           behavior:"smooth"
         })
       } 
     } else if (width > 690) {
       if (div === "gallery") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:940,
           behavior:"smooth"
         })
       } else if (div === "about") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:2200,
           behavior:"smooth"
         })
       } else {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:5250,
           behavior:"smooth"
         })
       }
     } else if (width > 455) {
       if (div === "gallery") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:740,
           behavior:"smooth"
         })
       } else if (div === "about") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:2480,
           behavior:"smooth"
         })
       } else {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:5440,
           behavior:"smooth"
         })
       }
     } else if (width > 360) {
       if (div === "gallery") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:740,
           behavior:"smooth"
         })
       } else if (div === "about") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:2480,
           behavior:"smooth"
         })
       } else {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:5560,
           behavior:"smooth"
         })
       }
     } else if (width > 314) {
       if (div === "gallery") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:740,
           behavior:"smooth"
         })
       } else if (div === "about") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:2480,
           behavior:"smooth"
         })
       } else {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:5510,
           behavior:"smooth"
         })
       }
     } else {
       if (div === "gallery") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:740,
           behavior:"smooth"
         })
       } else if (div === "about") {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:2445,
           behavior:"smooth"
         })
       } else {
-        document.getElementById("wrapper").scrollTo({
+        window.scrollTo({
           top:5600,
           behavior:"smooth"
         })
@@ -308,17 +308,7 @@ class Home extends React.Component {
     e.preventDefault();
     if (emailTest.test(this.state.email.toLowerCase()) && !(document.getElementById("submitbutton").className.includes("active"))) {
       document.getElementById("submitbutton").classList.toggle("active");
-      /*Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "sterlin.velazquez37@gmail.com",
-        Password : "F858F773A3DC6E81629BC9DA194AB27B3802",
-        To : 'sterlin.velazquez37@gmail.com',
-        From : this.state.email,
-        Subject : "Request from " + this.state.name,
-        Body : this.state.message,
-      }).then(
-        message => alert(message)
-      );*/
+        //
     }
   }
   resetInputs() {
